ef8ef111eb63495be12161042a5bf45825c22509
AmirUpSkill <amirabdallahpfe@gmail.com>
2026-04-14 14:19:43 +0100

    Feat : Add the  Log Picker Animation in the Pricing Page

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,10 +1,12 @@
 import { PageHero } from '../components/PageHero'
-import { pricingHero } from '../content/landingPage'
+import { LogoTicker } from '../components/LogoTicker'
+import { pricingHero, trustLogos } from '../content/landingPage'
 
 export function PricingPage() {
   return (
     <>
       <PageHero content={pricingHero} />
+      <LogoTicker title="Analysis you can trust." logos={trustLogos} />
       {/* We will add the actual pricing cards/tables down here next! */}
     </>
   )
