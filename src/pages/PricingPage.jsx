c11346802a66320cb74769f40703339c849e1009
AmirUpSkill <amirabdallahpfe@gmail.com>
2026-04-17 11:01:32 +0100

    Feat : remove the Comparision Section here

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,10 +1,9 @@
-import { ComparisonSection } from '../components/ComparisonSection'
 import { FreeTrialSection } from '../components/FreeTrialSection'
 import { PricingSection } from '../components/PricingSection'
 import { LargerScalePlansSection } from '../components/LargerScalePlansSection'
 import { PricingContactCtaSection } from '../components/PricingContactCtaSection'
 import { WhyChooseQInsightsSection } from '../components/WhyChooseQInsightsSection'
-import { freeTrialSection, pricingSection, largerScalePlansSection, gettingStartedSection, pricingComparisonSection, whyChooseQInsightsSection, pricingContactCtaSection } from '../content/landingPage'
+import { freeTrialSection, pricingSection, largerScalePlansSection, gettingStartedSection, whyChooseQInsightsSection, pricingContactCtaSection } from '../content/landingPage'
 
 export function PricingPage() {
   return (
@@ -12,7 +11,6 @@ export function PricingPage() {
       <PricingSection content={pricingSection} />
       <FreeTrialSection content={freeTrialSection} />
       <LargerScalePlansSection content={largerScalePlansSection} gettingStarted={gettingStartedSection} />
-      <ComparisonSection content={pricingComparisonSection} />
       <WhyChooseQInsightsSection content={whyChooseQInsightsSection} />
       <PricingContactCtaSection content={pricingContactCtaSection} />
     </>
