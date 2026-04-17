2ad497b99e0a9643b4aa36c9b76fd80bd8061858
AmirUpSkill <amirabdallahpfe@gmail.com>
2026-04-17 10:45:38 +0100

    Feat : Remove the Logo Picker in the Landing Page

--- src/pages/PricingPage.jsx
+++ src/pages/PricingPage.jsx
@@ -1,16 +1,14 @@
-import { LogoTicker } from '../components/LogoTicker'
 import { ComparisonSection } from '../components/ComparisonSection'
 import { FreeTrialSection } from '../components/FreeTrialSection'
 import { PricingSection } from '../components/PricingSection'
 import { LargerScalePlansSection } from '../components/LargerScalePlansSection'
 import { PricingContactCtaSection } from '../components/PricingContactCtaSection'
 import { WhyChooseQInsightsSection } from '../components/WhyChooseQInsightsSection'
-import { trustLogos, freeTrialSection, pricingSection, largerScalePlansSection, gettingStartedSection, pricingComparisonSection, whyChooseQInsightsSection, pricingContactCtaSection } from '../content/landingPage'
+import { freeTrialSection, pricingSection, largerScalePlansSection, gettingStartedSection, pricingComparisonSection, whyChooseQInsightsSection, pricingContactCtaSection } from '../content/landingPage'
 
 export function PricingPage() {
   return (
     <>
-      <LogoTicker title="Analysis you can trust." logos={trustLogos} />
       <FreeTrialSection content={freeTrialSection} />
       <PricingSection content={pricingSection} />
       <LargerScalePlansSection content={largerScalePlansSection} gettingStarted={gettingStartedSection} />
