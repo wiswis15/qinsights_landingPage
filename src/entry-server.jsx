24b0f4abccf52cc497ec43e2712ae1c44deef9e0
Wissem Golli <golliwissem@hotmail.com>
2026-05-26 15:49:23 +0200

    Fix import path for StaticRouter in entry-server.jsx

--- src/entry-server.jsx
+++ src/entry-server.jsx
@@ -1,5 +1,5 @@
 import { renderToString } from 'react-dom/server'
-import { StaticRouter } from 'react-router'
+import { StaticRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { getSeoRoute, seoRoutes } from './seo/routes'
 
@@ -12,4 +12,3 @@ export function render(url) {
     </StaticRouter>,
   )
 }
-
